67339b8206e9a36850405d3936ab2c7e1b4e2fd7
Lahiru Madushanka <lahirumadushandl@gmail.com>
2023-03-17 17:14:07 +0530

    Create register login functions

--- BookingApp/api/index.js
+++ BookingApp/api/index.js
@@ -1,24 +1,66 @@
 
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+require('dotenv').config()
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'ddddkfjgmlllweffmssssssllfgjjgjgkgk';
 
 app.use(express.json())
 
 app.use(cors({
     credentials: true,
-    origin:'http://localhost:5173',
+    origin: 'http://localhost:5173',
 }
-    
+
 
 ));
 
-app.get('/test',(req,res) =>{
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
     res.json('test ok');
 })
 
-app.post('/register',(req,res)=>{
-    const{name,email,password} = req.body;
-    res.json({name,email,password});
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        })
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+
+    
 });
+
+app.post('/login',async (req,res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({email:userDoc.email, id:userDoc._id},jwtSecret,{},(err,token)=>{
+                if (err) throw err;
+                res.cookie('token',token).json(userDoc);
+            })
+            
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.status(422).json('not found')
+    }
+})
+
 app.listen(4000);
